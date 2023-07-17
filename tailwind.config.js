7bbadfbfaf075d39256d249a7f3eea7c078d2c70
Jonash Gania <jonashtiglao@gmail.com>
2023-07-18 01:05:31 +0800

    Added custom theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,13 +5,21 @@ module.exports = {
     extend: {
       colors: {
         'dark-color': '#1E293B',
-        'light-color': 'EDEDED'
+        'light-color': '#EDEDED',
+        'light-font': 'rgba(226,232,240,1)',
+        'timeline': 'rgba(225,225,225, 9)'
       },
       borderRadius: {
         'circle': '50%'
       },
       fontFamily: {
         'inter': ["'Inter'", 'sans-serif']
+      },
+      boxShadow: {
+        'grid-content': '0 2rem 3rem rgba(132, 139, 200, 0.3)'
+      },
+      spacing: {
+        '22': '-90px',
       }
     },
   },
