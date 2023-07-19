ef197248cc4cee673bf1737128e9395f759b7541
Jonash Gania <jonashtiglao@gmail.com>
2023-07-19 15:03:15 +0800

    Fix sidebar menu toggle

--- js/main.js
+++ js/main.js
@@ -1,3 +1,111 @@
+/* TOGGLE SIDEBAR MENU */
+
+function toggleSidebarMenu(){
+    const sidebarMenuBtn = document.querySelector('.sidebar-menu-btn');
+    const sidebarMenu = document.querySelector('.sidebar-menu');
+    const closeBtn = document.querySelector('.close-btn');
+    const menuOverlay = document.querySelector('.overlay');
+    
+
+    sidebarMenuBtn.addEventListener('click', () => {
+        sidebarMenu.classList.add('active');
+        menuOverlay.classList.add('active');
+        document.body.style.overflow = 'hidden';
+
+    })
+    closeBtn.addEventListener('click', () => {
+        sidebarMenu.classList.remove('active');
+        menuOverlay.classList.remove('active');
+        document.body.style.overflow = 'auto';
+        
+    })
+    menuOverlay.addEventListener('click', () => {
+        sidebarMenu.classList.remove('active');
+        menuOverlay.classList.remove('active');
+        document.body.style.overflow = 'auto';
+        
+    })
+    function closeSidebarOnResize() {
+        if (window.innerWidth > 950 && sidebarMenu.classList.contains('active')) {
+          sidebarMenu.classList.remove('active');
+          menuOverlay.classList.remove('active');
+          document.body.style.overflow = 'auto';
+        } 
+      }
+    
+      window.addEventListener('resize', closeSidebarOnResize);
+   
+}
+toggleSidebarMenu();
+
+
+
+
+/* TOGGLE DARK MODE/LIGHT MODE*/
+
+const sun = document.querySelector('.sun');
+const moon = document.querySelector('.moon');
+const userTheme = localStorage.getItem("theme");
+const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+const iconToggle = () => {
+    sun.classList.toggle('display-none');
+    moon.classList.toggle('display-none');
+}
+
+const themeCheck = () => {
+    if(userTheme === "dark" || (!userTheme && systemTheme)){
+        document.documentElement.classList.add('dark');
+        moon.classList.add('display-none')
+        return;
+    }
+    sun.classList.add('display-none');
+}
+
+const themeSwitch = () => {
+    if(document.documentElement.classList.contains("dark")){
+        document.documentElement.classList.remove('dark');
+        localStorage.setItem("theme", "light");
+        iconToggle();
+        return;       
+    }
+    document.documentElement.classList.add("dark");
+    localStorage.setItem("theme", "dark");
+    iconToggle();
+}
+
+sun.addEventListener('click', () => {
+    themeSwitch();
+})
+moon.addEventListener('click', () => {
+    themeSwitch();
+})
+themeCheck();
+
+
+/* TOGGLE DROPDOWN PROFILE */
+
+function toggleDropdownProfile(){
+    const profile = document.querySelector('.profile');
+    const dropdownProfile = document.querySelector('.dropdown-profile');
+    
+    profile.addEventListener('click', () => {
+        dropdownProfile.classList.toggle('open');
+    })
+    document.addEventListener('click', (event) => {
+        const isClickInside = dropdownProfile.contains(event.target) || profile.contains(event.target);
+        
+        if (!isClickInside) {
+          dropdownProfile.classList.remove('open');
+        }
+    });
+}
+toggleDropdownProfile();
+
+
+
+/* TOGGLE SIDEBAR MINIMIZ */
+
 const sidebarToggle = document.querySelector('.sidebar-toggle');
 const sidebar = document.querySelector('.sidebar');
 const leftArrow = document.querySelector('.left-arrow');
@@ -22,6 +130,9 @@ function arrowChange(){
     }
 }
 
+
+/* PROGRESS BAR CHART */
+
 let progressBar = document.querySelector('.circular-progress');
 let progressValue = document.querySelector('.progress-value');
 
@@ -42,6 +153,9 @@ let progress = setInterval(() => {
 }, speed)
 
 
+
+/* DOUGHNUT CHART */
+
 const doughnutChart = document.querySelector('.myChart');
 
 new Chart(doughnutChart, {
@@ -57,7 +171,6 @@ new Chart(doughnutChart, {
         ]
     },
     options: {
-        borderWidth: 5,
         hoverBorderWidth: 0,
         plugins: {
             legend: {
